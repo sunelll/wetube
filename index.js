7970256353c977034089d2be4107401559136813
sunelll <hs0647@naver.com>
2020-07-21 17:18:26 +0900

    0720
    commit

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-console.log("hi");
+import "core-js"
+import express from "express";
+const app = express();
+
+const PORT = 4000;
+
+function handleListening(){
+    console.log(`Listening on: http://localhost:${PORT}`);
+}
+
+function handleHome(req, res){
+    res.send("hello from home");
+}
+
+function handleProfile(req, res){
+    res.send("You are on my Profile");
+}
+
+app.get("/", handleHome);
+app.get("/profile", handleProfile)
+
+app.listen(PORT, handleListening);
+
