8ba2c7ad011b97fb0f98f205d4f864cd7c4181de
sunelll <hs0647@naver.com>
2020-07-28 19:53:47 +0900

    0728
    commit

--- routers/videoRouter.js
+++ routers/videoRouter.js
@@ -1,15 +1,22 @@
 import express from "express";
 import routes from "../routes";
-import { getUpload, postUpload, videoDetail, editVideo, deleteVideo} from "../controller/videoController"
+import { getUpload, postUpload, videoDetail, getEditVideo, deleteVideo, postEditVideo} from "../controller/videoController"
 import { uploadVideo } from "../middlewares";
 
 const videoRouter = express.Router();
 
+//Upload
 videoRouter.get(routes.upload, getUpload);
 videoRouter.post(routes.upload, uploadVideo, postUpload);
-videoRouter.get(routes.videoDetail(), videoDetail);
-videoRouter.get(routes.editVideo, editVideo);
-videoRouter.get(routes.deleteVideo, deleteVideo);
 
+//Video Detail
+videoRouter.get(routes.videoDetail(), videoDetail);
+
+//Edit video
+videoRouter.get(routes.editVideo(), getEditVideo);
+videoRouter.post(routes.editVideo(), postEditVideo);
+
+//Delete video
+videoRouter.get(routes.deleteVideo(), deleteVideo);
 
 export default videoRouter;
