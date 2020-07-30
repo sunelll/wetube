2edb48c0f7141f4f5e4264ccd7b149a55d6c93d8
sunelll <hs0647@naver.com>
2020-07-30 18:34:28 +0900

    0730
    commit

--- app.js
+++ app.js
@@ -4,29 +4,29 @@ import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
 import { localsMiddleware } from "./middlewares";
-import userRouter  from "./routers/userRouter";
+import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
 import globalRouter from "./routers/globalRouter";
-import routes from "./routes"
+import routes from "./routes";
 const app = express();
 
 app.use(helmet());
-app.set("views", __dirname + "/views")
-app.set("view engine","pug");
+app.set("views", __dirname + "/views");
+app.set("view engine", "pug");
 app.use("/uploads", express.static("uploads"));
+app.use("/static", express.static("static"));
 app.use(cookieParser());
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(morgan("dev"));
-app.use(localsMiddleware)
+app.use(localsMiddleware);
 
 app.use(routes.home, globalRouter);
 app.use(routes.users, userRouter);
 app.use(routes.videos, videoRouter);
 
-
-app.get('/change-password2', function(req, res) {
-    res.end('change-password2');
+app.get("/change-password2", function (req, res) {
+  res.end("change-password2");
 });
 
 export default app;
--- webpack.config.js
+++ webpack.config.js
@@ -1,41 +1,45 @@
 const path = require("path");
-const autoprefixer = require("autoprefixer");
-const ExtractCSS = require("extract-text-webpack-plugin");
+// import path from "path"
+//const autoprefixer = require("autoprefixer");
+//const ExtractCSS = require("extract-text-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 const MODE = process.env.WEBPACK_ENV;
 const ENTRY_FILE = path.resolve(__dirname, "assets", "js", "main.js");
 const OUTPUT_DIR = path.join(__dirname, "static");
 
 const config = {
-  entry: ENTRY_FILE,
+  entry: ["@babel/polyfill", ENTRY_FILE],
   mode: MODE,
+  output: {
+    path: OUTPUT_DIR,
+    filename: "[name].js",
+    publicPath: "/static/react/dist/",
+  },
+  stats: {
+    entrypoints: false,
+    children: false,
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "styles.css",
+    }),
+  ],
   module: {
     rules: [
       {
-        test: /\.(scss)$/,
-        use: ExtractCSS.extract([
+        test: /\.(js)$/,
+        use: [
           {
-            loader: "css-loader",
+            loader: "babel-loader",
           },
-          {
-            loader: "postcss-loader",
-            options: {
-              plugins() {
-                return [autoprefixer];
-              },
-            },
-          },
-          {
-            loader: "sass-loader",
-          },
-        ]),
+        ],
+      },
+      {
+        test: /\.scss$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
       },
     ],
-  },
-  plugins: [new ExtractTextPlugin("styles.css")],
-  output: {
-    path: OUTPUT_DIR,
-    filename: "[name].[format]",
   },
 };
 
