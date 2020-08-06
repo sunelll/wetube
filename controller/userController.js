6fe9793219fe3c646952efd121a6b11e4e532e5f
sunelll <hs0647@naver.com>
2020-08-06 18:35:30 +0900

    0806
    commit

--- controller/userController.js
+++ controller/userController.js
@@ -40,12 +40,14 @@ export const githubLogin = passport.authenticate("github");
 
 export const githubLoginCallback = async (_, __, profile, cb) => {
   const {
-    _json: { id, avatar_url: avatarUrl, name, email },
+    _json: { id, avatar_url, name, email },
   } = profile;
   try {
     const user = await User.findOne({ email });
     if (user) {
       user.githubId = id;
+      user.avatarUrl = avatar_url;
+      user.name = name;
       user.save();
       return cb(null, user);
     }
